4d411829b8c916c51fa32dd20a2bba9c18ee6ac3
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2025-03-16 17:47:53 -0500

    Added more infrastructure.

---
+++ OpenGL/Pow/Systems/Render.cs
@@ -0,0 +1,82 @@
+﻿using Arch.Core;
+using Arch.System;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using MonoGame.Extended.Collisions.Layers;
+using Pow.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pow.Systems.Render
+{
+    internal class UpdateSystem : BaseSystem<World, GameTime>
+    {
+        
+        private readonly UpdateDraw _parent;
+        public UpdateSystem(UpdateDraw parent) : base(parent.World) 
+        {
+            _parent = parent;
+        }
+        public override void Update(in GameTime t)
+        {
+          
+            var map = _parent.Map;
+
+            map.Update();
+
+            base.Update(t);
+        }
+    }
+    internal class DrawSystem : BaseSystem<World, GameTime>
+    {
+        private readonly static Layers[] _layers = Enum.GetValues<Layers>();
+        private readonly UpdateDraw _parent;
+        public DrawSystem(UpdateDraw parent) : base(parent.World)
+        {
+            _parent = parent;
+        }
+        public override void Update(in GameTime t)
+        {
+            ref var view = ref _parent.Camera.View;
+            var map = _parent.Map;
+
+            foreach (ref var layer in _layers.AsSpan())
+            {
+                map.Draw(in layer, in view);
+            }
+            base.Update(t);
+        }
+    }
+    internal class UpdateDraw : IDisposable
+    {
+        private readonly World _world;
+        private readonly Camera _camera;
+        private readonly Map _map;
+        private readonly UpdateSystem _updateSystem;
+        private readonly DrawSystem _drawSystem;
+        public World World => _world;
+        public Camera Camera => _camera;
+        public Map Map => _map;
+        public UpdateSystem UpdateSystem => _updateSystem;
+        public DrawSystem DrawSystem => _drawSystem;
+        public UpdateDraw(World world, Camera camera, Map map)
+        {
+            _world = world;
+            _camera = camera;
+            _map = map;
+            _updateSystem = new(this);
+            _drawSystem = new(this);
+
+            _updateSystem.Initialize();
+            _drawSystem.Initialize();
+        }
+        public void Dispose()
+        {
+            _updateSystem.Dispose();
+            _drawSystem.Dispose();
+        }
+    }
+}
---
+++ OpenGL/Pow/Utilities/EnumsInterfaces.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pow.Utilities
+{
+    public enum Layers { Background, Ground, Foreground }
+    public interface IRunnerParent
+    {
+        public void Initialize(Map map);
+    }
+}
