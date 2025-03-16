5299826e05fcc5c3d2e691722a94fc52d049a1da
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2025-03-15 22:50:12 -0500

    Added Arch. Implemented a modified version of the TiledMapRenderer to remove any dynamic allocations.

--- OpenGL/Game1.cs
+++ OpenGL/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Tiled;
+using Pow;
 
 namespace OpenGLGame
 {
@@ -8,6 +10,8 @@ namespace OpenGLGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private TiledMap _tiledMap;
+        private TiledMapRenderer _tiledMapRenderer;
 
         public Game1()
         {
@@ -18,25 +22,29 @@ namespace OpenGLGame
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            Globals.InitializeMonoGame(spriteBatch: _spriteBatch, contentManager: Content);
+            Globals.InitializePow();
+            _tiledMap = Content.Load<TiledMap>("tiled/test_map_0");
+            _tiledMapRenderer = new(GraphicsDevice, _tiledMap);
         }
-
+        protected override void EndRun()
+        {
+            base.EndRun();
+            Globals.Dispose();
+        }
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
-
+            _tiledMapRenderer.Update(gameTime);
             base.Update(gameTime);
         }
 
@@ -45,6 +53,7 @@ namespace OpenGLGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _tiledMapRenderer.Draw();
 
             base.Draw(gameTime);
         }
---
+++ OpenGL/Pow/Globals.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pow
+{
+    public static class Globals
+    {
+        private static States _state = States.WaitingForInitMG;
+        private static SpriteBatch _spriteBatch;
+        private static ContentManager _contentManager;
+        private static Runner _runner;
+        public enum States { WaitingForInitMG, WaitingForInitPow, GameRunning, Disposed }
+        public static SpriteBatch SpriteBatch
+        {
+            get
+            {
+                Debug.Assert(_state == States.GameRunning);
+                return _spriteBatch;
+            }
+        }
+        public static ContentManager ContentManager
+        {
+            get
+            {
+                Debug.Assert(_state == States.GameRunning);
+                return _contentManager;
+            }
+        }
+        public static States State => _state;
+        public static void InitializeMonoGame(SpriteBatch spriteBatch, ContentManager contentManager)
+        {
+            Debug.Assert(_state == States.WaitingForInitMG);
+            _spriteBatch = spriteBatch;
+            _contentManager = contentManager;
+            _state = States.WaitingForInitPow;
+        }
+        public static void InitializePow()
+        {
+            Debug.Assert(_state == States.WaitingForInitPow);
+            _runner = new();
+            _state = States.GameRunning;
+        }
+        public static void Dispose()
+        {
+            Debug.Assert(_state == States.GameRunning);
+            _runner.Dispose();
+            _state = States.Disposed;
+        }
+    }
+}
