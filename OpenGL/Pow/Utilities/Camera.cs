66266e90c681b9a0a85fb214d06a2a49ed160d53
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2025-03-16 20:36:46 -0500

    Cleaned up the map and updated the renderer. Gonna work on the sprites next.

--- OpenGL/Pow/Utilities/Camera.cs
+++ OpenGL/Pow/Utilities/Camera.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
+using MonoGame.Extended;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +16,11 @@ namespace Pow.Utilities
         private float _rotation;
         private float _zoom;
         private float _pitch;
+        private Size _size;
         private Matrix _view;
+        private Matrix _projection;
+        private Matrix _viewProjection;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private void UpdateView()
         {
             _view = 
@@ -24,55 +30,95 @@ namespace Pow.Utilities
                 Matrix.CreateScale(_zoom, _zoom * _pitch, 1f) * 
                 Matrix.CreateTranslation(new Vector3(_origin, 0f));
         }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void UpdateProjection()
+        {
+            _projection = Matrix.CreateOrthographicOffCenter(0, _size.Width, _size.Height, 0, 0, -1); 
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void UpdateViewProjection()
+        {
+            _viewProjection = _view * _projection;
+        }
         public ref Matrix View => ref _view;
+        public ref Matrix Projection => ref _projection;
+        public ref Matrix ViewProjection => ref _viewProjection;
         public Vector2 Position
         {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => _position;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
                 if (_position == value) return;
                 _position = value;
                 UpdateView();
+                UpdateViewProjection();
             }
         }
         public Vector2 Origin
         {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => _origin;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
                 if (_origin == value) return;
                 _origin = value;
                 UpdateView();
+                UpdateViewProjection();
             }
         }
         public float Rotation
         {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => _rotation;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
                 if (_rotation == value) return;
                 _rotation = value;
                 UpdateView();
+                UpdateViewProjection();
             }
         }
         public float Zoom
         {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => _zoom;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
                 if (_zoom == value) return;
                 _zoom = value;
                 UpdateView();
+                UpdateViewProjection();
             }
         }
         public float Pitch
         {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             get => _pitch;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             set
             {
                 if (_pitch == value) return;
                 _pitch = value;
                 UpdateView();
+                UpdateViewProjection();
+            }
+        }
+        public Size Size
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _size;
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set
+            {
+                if (_size == value) return;
+                _size = value;
+                UpdateProjection();
+                UpdateViewProjection();
             }
         }
         public Camera()
@@ -83,6 +129,7 @@ namespace Pow.Utilities
             _zoom = 1;
             _pitch = 1;
             UpdateView();
+            UpdateProjection();
         }
     }
 }
