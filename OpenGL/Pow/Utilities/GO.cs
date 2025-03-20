cb82f10338173d514fabdaafc1247a761eadebc4
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2025-03-19 23:19:44 -0500

    Trying to figure out a good system for managing / preallocating game objects in a convenient way. Will work on this tomorrow.

---
+++ OpenGL/Pow/Utilities/GO.cs
@@ -0,0 +1,35 @@
+﻿using Pow.Utilities.Animations;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pow.Utilities.GO
+{
+    public interface IGOManager
+    {
+        public void Return();
+    }
+    public interface IGOGenerator<T> where T : IGOManager
+    {
+        public void Initialize(int capacity);
+        public T Acquire();
+    }
+    public class GOContainer
+    {
+        private readonly Dictionary<Type, IGOGenerator<IGOManager>> _generators = [];
+        public void Add<T>() where T : IGOGenerator<IGOManager>, new()
+        {
+            var type = typeof(T);
+            Debug.Assert(!_generators.ContainsKey(type));
+            _generators.Add(type, new T());
+        }
+        public T Get<T>() where T : IGOGenerator<IGOManager>
+        {
+            var type = typeof(T);
+            return (T)_generators[type];
+        }
+    }
+}
