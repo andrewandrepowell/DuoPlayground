8f76758aa314f8621f26052543bc21f9bd900b17
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2025-03-24 01:56:28 -0500

    Redid the initialize and destroy systems without the Get and Set with ComponentType input and removed a lot of allocations. I'm gonna completely remove the Command Buffer with my own Queue next.

--- OpenGL/Pow/Systems/DestroySystem.cs
+++ OpenGL/Pow/Systems/DestroySystem.cs
@@ -1,10 +1,13 @@
 ﻿using Arch.Core;
 using Arch.Core.Extensions;
+using Arch.Core.Utils;
 using Arch.System;
 using Microsoft.Xna.Framework;
 using Pow.Components;
+using Pow.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,19 +17,46 @@ namespace Pow.Systems
     internal class DestroySystem : BaseSystem<World, GameTime>
     {
         private readonly QueryDescription _queryDescription = new QueryDescription().WithAll<StatusComponent>();
-        private readonly ForEach _destroyComponents = new((Entity entity) => DestroyComponents(in entity));
-        private static void DestroyComponents(in Entity entity)
+        private readonly ForEach _destroyComponents;
+        private readonly Dictionary<ComponentType, IDispose> _componentNodes = [];
+        private bool _initialized = false;
+        private interface IDispose
+        {
+            public void Dispose(in Entity entity);
+        }
+        private record ComponentNode<T>(World World) : IDispose where T : IDisposable
+        {
+            public void Dispose(in Entity entity)
+            {
+                ref var component = ref World.Get<T>(entity);
+                component.Dispose();
+            }
+        }
+        private void DestroyComponents(in Entity entity)
         {
             ref var statusComponent = ref entity.Get<StatusComponent>();
-            if (statusComponent.State != Utilities.EntityStates.Destroying)
+            if (statusComponent.State != EntityStates.Destroying)
                 return;
-            foreach (ref var componentType in entity.GetComponentTypes().AsSpan())
-                if (entity.Get(componentType) is IDisposable disposable)
-                    disposable.Dispose();
-            statusComponent.State = Utilities.EntityStates.Destroyed;
+            foreach (ref var componentType in World.GetComponentTypes(entity).AsSpan())
+                if (_componentNodes.TryGetValue(componentType, out var componentNode))
+                    componentNode.Dispose(in entity);
+            statusComponent.State = EntityStates.Running;
+        }
+        public void Add<T>() where T : IDisposable
+        {
+            Debug.Assert(!_initialized);
+            var componentType = (ComponentType)typeof(T);
+            Debug.Assert(!_componentNodes.ContainsKey(componentType));
+            _componentNodes.Add(componentType, new ComponentNode<T>(World));
         }
         public DestroySystem(World world) : base(world) 
-        { 
+        {
+            _destroyComponents = new((Entity entity) => DestroyComponents(in entity));
+        }
+        public override void Initialize()
+        {
+            base.Initialize();
+            _initialized = true;
         }
         public override void Update(in GameTime t)
         {
