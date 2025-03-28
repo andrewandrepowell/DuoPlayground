c3f383f44b72e65007dce9dc66828efb3788b3f5
Andrew Andre Powell <andrewandrepowell2@gmail.com>
2025-03-27 23:06:17 -0500

    Very nice. Got some basic physics implemented, and the general structure feels mostly good. Might start working on implementing the actual Duo structure instead of just doing pure testing.

--- OpenGL/Game1.cs
+++ OpenGL/Game1.cs
@@ -12,13 +12,56 @@ using Arch.Core.Extensions;
 using MonoGame.Extended;
 using System.Collections;
 using System.Diagnostics;
+using System.Text;
+using System.Xml.Linq;
+using System.Linq;
+using Pow.Utilities.Animations;
+using nkast.Aether.Physics2D.Dynamics;
+using EcsWorld = Arch.Core.World;
 
 
 namespace OpenGLGame
 {
     public enum Maps { LevelDebug0 }
     public enum Animations { BallDebug0, BallDebug1 }
-    public enum EntityTypes { Initialize, Wall }
+    public enum EntityTypes { Initialize, Wall, Ball }
+    public class BallGOManager : GOCustomManager
+    {
+        private AnimationManager _animationManager;
+        private Body _physicsBody;
+        private bool _initialized = false;
+        public void Initialize(Map.PolygonNode node)
+        {
+            Debug.Assert(!_initialized);
+            {
+                _physicsBody = Entity.Get<PhysicsComponent>().Manager.Body;
+                _physicsBody.BodyType = BodyType.Dynamic;
+                _physicsBody.Mass = 1;
+                var position = node.Vertices.Average() + node.Position;
+                _physicsBody.CreateCircle(radius: 15, density: 1);
+                _physicsBody.Position = position;
+            }
+            {
+                _animationManager = Entity.Get<AnimationComponent>().Manager;
+                _animationManager.Play((int)Animations.BallDebug0);
+            }
+            _initialized = true;
+        }
+        public override void Update()
+        {
+            Debug.Assert(_initialized);
+            if (!_animationManager.Running)
+                _animationManager.Play((int)Animations.BallDebug0);
+            _physicsBody.ApplyForce(new Vector2(0, 20000));
+            base.Update();
+        }
+        public override void Cleanup()
+        {
+            Debug.Assert(_initialized);
+            _initialized = false;
+            base.Cleanup();
+        }
+    }
     public class WallGOManager : GOCustomManager
     {
         private bool _initialized = false;
@@ -26,19 +69,23 @@ namespace OpenGLGame
         {
             Debug.Assert(!_initialized);
             var body = Entity.Get<PhysicsComponent>().Manager.Body;
-            body.BodyType = nkast.Aether.Physics2D.Dynamics.BodyType.Static;
+            body.BodyType = BodyType.Static;
+            body.Mass = 1;
             body.CreatePolygon(vertices: new(node.Vertices), density: 1);
             body.Position = node.Position;
             _initialized = true;
+        }
+        public override void Cleanup()
+        {
+            Debug.Assert(false);
+            base.Cleanup();
         }
     }
     public class InitializeGOManager : GOCustomManager
     {
         private static InitializeGOManager _manager;
         private readonly Queue<Map.PolygonNode> _polygonNodes = [];
-        private readonly Queue<Entity> _entities = [];
-        public Queue<Map.PolygonNode> PolygonNodes => _polygonNodes;
-        public Queue<Entity> Entities => _entities;
+        private readonly Queue<Entity> _polygonEntities = [];
         public static InitializeGOManager Manager => _manager;
         public override void Initialize()
         {
@@ -48,16 +95,28 @@ namespace OpenGLGame
         }
         public override void Update()
         {
-            while (_entities.TryDequeue(out var entity))
+            while (_polygonEntities.TryDequeue(out var entity))
             {
-                if (entity.Has<GOCustomComponent<WallGOManager>>())
+                var node = _polygonNodes.Dequeue();
+                switch (Enum.Parse<EntityTypes>(node.Parameters["EntityType"]))
                 {
-                    var manager = entity.Get<GOCustomComponent<WallGOManager>>().Manager;
-                    var polygonNode = _polygonNodes.Dequeue();
-                    manager.Initialize(polygonNode);
+                    case EntityTypes.Wall:
+                        entity.Get<GOCustomComponent<WallGOManager>>().Manager.Initialize(node);
+                        break;
+                    case EntityTypes.Ball:
+                        entity.Get<GOCustomComponent<BallGOManager>>().Manager.Initialize(node);
+                        break;
+                    default:
+                        Debug.Assert(false); 
+                        break;
                 }
             }
             base.Update();
+        }
+        public void Add(Map.PolygonNode node)
+        {
+            Globals.Runner.CreateEntity((int)Enum.Parse<EntityTypes>(node.Parameters["EntityType"]), _polygonEntities);
+            _polygonNodes.Enqueue(node);
         }
     }
     public class Game1 : Game, IRunnerParent
@@ -83,18 +142,18 @@ namespace OpenGLGame
             runner.AnimationGenerator.ConfigureAnimation((int)Animations.BallDebug1, 0, 1, [0, 4, 5], 0.25f, false);
             runner.AddGOCustomManager<InitializeGOManager>();
             runner.AddGOCustomManager<WallGOManager>();
-            runner.AddEntityType((int)EntityTypes.Initialize, (World world) => world.Create(new StatusComponent(), new GOCustomComponent<InitializeGOManager>()));
-            runner.AddEntityType((int)EntityTypes.Wall, (World world) => world.Create(new StatusComponent(), new PhysicsComponent(), new GOCustomComponent<WallGOManager>()));
+            runner.AddGOCustomManager<BallGOManager>();
+            runner.AddEntityType((int)EntityTypes.Initialize, (EcsWorld world) => world.Create(new StatusComponent(), new GOCustomComponent<InitializeGOManager>()));
+            runner.AddEntityType((int)EntityTypes.Wall, (EcsWorld world) => world.Create(new StatusComponent(), new PhysicsComponent(), new GOCustomComponent<WallGOManager>()));
+            runner.AddEntityType((int)EntityTypes.Ball, (EcsWorld world) => world.Create(new StatusComponent(), new AnimationComponent(), new PhysicsComponent(), new GOCustomComponent<BallGOManager>()));
+
         }
         public void Initialize(Map.MapNode node)
         {
             var initialize = InitializeGOManager.Manager;
             var runner = Globals.Runner;
-            foreach (var polygonNode in node.PolygonNodes)
-            {
-                runner.CreateEntity((int)EntityTypes.Wall, initialize.Entities);
-                initialize.PolygonNodes.Enqueue(polygonNode);
-            }
+            foreach (ref var polygonNode in node.PolygonNodes.AsSpan())
+                initialize.Add(polygonNode);
         }
         protected override void LoadContent()
         {
@@ -103,8 +162,8 @@ namespace OpenGLGame
             Globals.InitializePow(this);
 
             Globals.Runner.Camera.Zoom = 2f;
-            //Globals.Runner.Camera.Position = new Vector2(-32, -32);
-            //Globals.Runner.Camera.Rotation = (float)Math.PI * 0.05f;
+            Globals.Runner.Camera.Position = new Vector2(-32, -32);
+            Globals.Runner.Camera.Rotation = (float)Math.PI * 0.05f;
 
             Globals.Runner.CreateEntity((int)EntityTypes.Initialize);
             Globals.Runner.Map.Load((int)Maps.LevelDebug0);
