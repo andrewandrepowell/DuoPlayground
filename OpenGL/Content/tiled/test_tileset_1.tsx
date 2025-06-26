5b511bb9dbef4248fed7ba8e91d9fc4051e4ea15
Andrew A Powell <andrewandrepowell2@gmail.com>
2025-06-26 02:12:48 -0500

    Did more work on the brick tiles. Gonna add all of them to the test level tomorrow.

---
+++ OpenGL/Content/tiled/test_tileset_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="test_tileset_1" tilewidth="16" tileheight="16" tilecount="512" columns="16">
+ <image source="../images/test_tileset_1.png" width="256" height="512"/>
+</tileset>
