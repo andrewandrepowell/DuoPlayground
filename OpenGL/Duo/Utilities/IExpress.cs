98dc902e58e0dcb95eb0dc9e71a1e256525141aa
Andrew A Powell <andrewandrepowell2@gmail.com>
2026-01-27 00:52:08 -0600

    Started the beginnings of the express system.

---
+++ OpenGL/Duo/Utilities/IExpress.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duo.Utilities;
+
+internal interface IExpress
+{
+    public class Node(string expression)
+    {
+        public string Expression { get; } = expression;
+        public bool Running { get; private set; } = false;
+        public bool Stopped { get; private set; } = false;
+        public bool Closed { get; private set; } = false;
+        public void Close()
+        {
+            Debug.Assert(!Stopped);
+            Closed = true;
+        }
+        public void Start()
+        {
+            Debug.Assert(!Stopped);
+            Debug.Assert(!Running);
+            Running = true;
+        }
+        public void Stop()
+        {
+            Debug.Assert(Running);
+            Debug.Assert(!Stopped);
+            Running = false;
+            Stopped = true;
+        }
+    }
+    public Node Submit(string expression);
+}
